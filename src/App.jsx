8d1789fcc1f17cd91b3f4901593aa6f622557de1
bangaffannn <bangaffannn@gmail.com>
2024-01-07 06:47:21 +0700

    Adding features and testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Cover from "./components/Cover";
+import Feature from "./components/Feature";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
     <>
       <Header />
       <Cover />
+      <Feature />
+      <Testimonials />
       <Footer />
     </>
   );
